dce7c1b0668cfed758b8b8629e1709d088dae9c1
Rohit Mehta <63762981+r0hitm@users.noreply.github.com>
2024-01-14 17:49:34 +0530

    minor config

--- _data/metadata.js
+++ _data/metadata.js
@@ -1,11 +1,11 @@
 module.exports = {
-	title: "Eleventy Base Blog v8",
-	url: "https://example.com/",
+	title: "DevAvatar",
+	url: "https://devavatar.com/",
 	language: "en",
 	description: "I am writing about my experiences as a naval navel-gazer.",
 	author: {
-		name: "Your Name Here",
-		email: "youremailaddress@example.com",
-		url: "https://example.com/about-me/"
+		name: "Rohit Mehta",
+		email: "rohitm09@proton.me",
+		url: "https://github.com/r0hitm" // todo: replace w this site's about page
 	}
 }
