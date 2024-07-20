9c6ff5e5506a3603528381a67426282508b9d7cb
Rohit Mehta <63762981+r0hitm@users.noreply.github.com>
2024-07-20 19:49:57 +0530

    update robots to disallow AI crawlers

--- src/pages/robots.txt.ts
+++ src/pages/robots.txt.ts
@@ -5,6 +5,70 @@ const robots = `
 User-agent: Googlebot
 Disallow: /nogooglebot/
 
+# GPTBot
+User-agent: GPTBot
+Disallow: /
+
+# ChatGPT-User
+User-agent: ChatGPT-User
+Disallow: /
+
+# Google-Extended
+User-agent: Google-Extended
+Disallow: /
+
+# PerplexityBot
+User-agent: PerplexityBot
+Disallow: /
+
+# Amazonbot
+User-agent: Amazonbot
+Disallow: /
+
+# ClaudeBot
+User-agent: ClaudeBot
+Disallow: /
+
+# Omgilibot
+User-agent: Omgilibot
+Disallow: /
+
+# FacebookBot
+User-agent: FacebookBot
+Disallow: /
+
+# Applebot
+User-agent: Applebot
+Disallow: /
+
+# anthropic-ai
+User-agent: anthropic-ai
+Disallow: /
+
+# Bytespider
+User-agent: Bytespider
+Disallow: /
+
+# Claude-Web
+User-agent: Claude-Web
+Disallow: /
+
+# Diffbot
+User-agent: Diffbot
+Disallow: /
+
+# ImagesiftBot
+User-agent: ImagesiftBot
+Disallow: /
+
+# Omgili
+User-agent: Omgili
+Disallow: /
+
+# YouBot
+User-agent: YouBot
+Disallow: /
+
 User-agent: *
 Disallow: /tags/
 Allow: /$
