a9a71bb2212ec9da12db973ed1e596384e541ec5
Rohit Mehta <63762981+r0hitm@users.noreply.github.com>
2025-03-20 10:51:29 -0700

    update + autofocus search

--- src/components/Search.tsx
+++ src/components/Search.tsx
@@ -83,17 +83,14 @@ export default function SearchBar({ searchList }: Props) {
           <span className="sr-only">Search</span>
         </span>
         <input
-          className="block w-full rounded border border-skin-fill 
-        border-opacity-40 bg-skin-fill py-3 pl-10
-        pr-3 placeholder:italic placeholder:text-opacity-75 
-        focus:border-skin-accent focus:outline-none"
-          placeholder="Search for anything..."
+          className="block w-full rounded border border-skin-fill border-opacity-40 bg-skin-fill py-3 pl-10 pr-3 placeholder:italic placeholder:text-opacity-75 focus:border-skin-accent focus:outline-none"
+          placeholder="how to do..."
           type="text"
           name="search"
           value={inputVal}
           onChange={handleChange}
           autoComplete="off"
-          // autoFocus
+          autoFocus
           ref={inputRef}
         />
       </label>
