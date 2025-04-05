7c11e4649eace6441f959ede8a379657032c8fe8
Rohit Mehta <63762981+r0hitm@users.noreply.github.com>
2025-04-05 06:27:44 -0700

    show even drafts in dev mode

--- src/utils/postFilter.ts
+++ src/utils/postFilter.ts
@@ -2,10 +2,13 @@ import { SITE } from "@config";
 import type { CollectionEntry } from "astro:content";
 
 const postFilter = ({ data }: CollectionEntry<"blog">) => {
+  const isDev = import.meta.env.DEV;
+
   const isPublishTimePassed =
     Date.now() >
     new Date(data.pubDatetime).getTime() - SITE.scheduledPostMargin;
-  return !data.draft && (import.meta.env.DEV || isPublishTimePassed);
+
+  return isDev ? true : !data.draft && isPublishTimePassed;
 };
 
 export default postFilter;
